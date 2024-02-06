15ee54459b689eeb9966f7caf179fcf8b9763959
velvet-jedi <pranjalruhela2811@gmail.com>
2024-02-06 23:16:57 +0530

    repeatString tested OK

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times < 0 ) return 'ERROR'
+    res = '';
+    for (let i = 0 ; i < times ; i++){
+        res += str;
+    }
+    return res;
 };
 
 // Do not edit below this line
