0354499cea7b29d4aaad847ae1bf68934d7a70e3
velvet-jedi <pranjalruhela2811@gmail.com>
2024-02-06 23:56:36 +0530

    reverstring tested OK

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    var reverse = [...str].reverse().join('');
+
+    return reverse;
 };
 
 // Do not edit below this line
