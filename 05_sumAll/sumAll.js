7bb8560f3eb8b340986b074b4adf0c05f5c51fd8
velvet-jedi <pranjalruhela2811@gmail.com>
2024-02-07 01:37:44 +0530

    sumAll tested OK

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (typeof(a) != 'number' || typeof(b) != 'number'){
+    return 'ERROR';
+  }  
 
+  if (a<0 || b<0){
+    return 'ERROR';
+  }  
+  
+  var sum = 0;
+
+  if (a > b ) {
+      var larger = a;
+      var smaller = b;
+  } else if (b > a) {
+      var larger = b;
+      var smaller = a;
+  }
+
+  for (i = smaller ; i < larger+1 ; i++){
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
