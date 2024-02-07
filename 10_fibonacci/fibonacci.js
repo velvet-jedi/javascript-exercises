5ac0d681a7a0da8022433975a9bb0bb8db8c66c7
velvet-jedi <pranjalruhela2811@gmail.com>
2024-02-07 16:49:02 +0530

    fibbo

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
 
+    if (a == 0 ) return 0;
+    if (a < 0) return 'OOPS';
+    var fibo = [];
+    for (let i = 0 ;  i < a ; i++){
+        if (i == 1 || i == 0){
+            fibo.push(1);
+        }
+        else {
+            fibo.push(fibo[i-1] + fibo[i-2]);
+        }
+    }
+    return fibo[a-1];
 };
 
 // Do not edit below this line
