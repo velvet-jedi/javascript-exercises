80b8d0baa9dec230f0133ef1066a6b65827471aa
velvet-jedi <pranjalruhela2811@gmail.com>
2024-02-07 16:31:37 +0530

    palindroms tested OK

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    // Compare the cleaned string with its reversed version
+    const reversedStr = cleanStr.split('').reverse().join('');
+    return cleanStr === reversedStr;
+    
 };
-
 // Do not edit below this line
 module.exports = palindromes;
+console.log(palindromes('Racecar!')); // Output: true
