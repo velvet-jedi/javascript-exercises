cb6ebae72779930ea262367fce3710a32b1b065a
velvet-jedi <pranjalruhela2811@gmail.com>
2024-02-07 15:27:31 +0530

    calculator tested OK

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return Math.abs(a-b);
+};
+
+const sum = function(arr) {
+  var sum = 0;
+  for (elem of arr) {
+    sum += elem;
+  }
+  return sum;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const multiply = function(arr) {
+  var prod = 1;
+  for (elem of arr) {
+    prod *= Number(elem);
+  }
+  return Number(prod);
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
+
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	var factor = a;
+  if (a == 1 || a == 0) return 1;
+  return factor * factorial(a-1);
+
 };
 
 // Do not edit below this line
