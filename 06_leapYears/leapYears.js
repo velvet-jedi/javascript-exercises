7879e140dc58cc930a99ca434acd09738453ec19
velvet-jedi <pranjalruhela2811@gmail.com>
2024-02-07 15:03:34 +0530

    leapyear tested OK

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    var flag = false;
+    if ((year%4===0 && year%100!=0) || year%400==0){
+        flag = true;
+    }
+    return flag;
 };
 
 // Do not edit below this line
