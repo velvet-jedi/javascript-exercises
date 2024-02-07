226649733214a819f09d508325fc1d628c4f3ec5
velvet-jedi <pranjalruhela2811@gmail.com>
2024-02-07 15:10:55 +0530

    temperatur units converter tested OK

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  var temp = temp;
+  var cel = (temp-32) * 1/1.8;
+  return Number(cel.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  var temp = temp;
+  var fah = (temp * 9/5) + 32;
+  return Number(fah.toFixed(1));
 };
 
 // Do not edit below this line
